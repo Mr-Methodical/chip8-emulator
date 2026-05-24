cf7e43d928b389e13f82e19402a0bea3602bef0d
Ryan Howe <r3howe@uwaterloo.ca>
2026-05-24 16:05:42 -0400

    Finished the LoadROM

--- chip8.cpp
+++ chip8.cpp
@@ -1,4 +1,5 @@
 #include "chip8.h"
+#include <stdexcept>
 #include <fstream>
 
 const unsigned int START_ADDRESS = 0x200;
@@ -12,9 +13,23 @@ Chip8::Chip8() { // this is the constructor
 void Chip8::LoadROM(char const *filename) {
   // open the file, this creates the connection to it
   // we read it only as binary and start at the end
-  std::ifstream file(filename, std::ios:binary | std::ios::ate);
+  std::ifstream file(filename, std::ios::binary | std::ios::ate);
   if (file.is_open()) {
     // we need to know size of file so we get tellg to us where the end is
     std::streampos size = file.tellg();
+    // we want a space large enough to hold ROM contents
+    char *buffer = new char[size];
+    // we need to go back to the beginning:
+    file.seekg(0, std::ios::beg);
+    // put the data into the buffer:
+    file.read(buffer, size);
+    file.close(); // no longer need it
+    if (size > (4096 - START_ADDRESS)) {
+      throw std::runtime_error("ROM too large!");
+    }
+    for (size_t i = 0; i < size; ++i) {
+      memory[START_ADDRESS + i] = buffer[i];
+    }
+    delete[] buffer;
   }
 }
