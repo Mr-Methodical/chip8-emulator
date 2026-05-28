c4a0aef9fe96cdd8e7a8b9c8ab72b86ec8b927df
Ryan Howe <r3howe@uwaterloo.ca>
2026-05-28 09:31:47 -0400

    added update processinput functions in platform to show the data on screeen and then also update to 'hardware' to reflect what key the user is typing

--- platform.cpp
+++ platform.cpp
@@ -41,12 +41,169 @@ void Platform::Update(void const *buffer, int pitch) {
                              // are updating the whole screen
                     buffer, // source pixel data
                     pitch);
-  SDL_RendererClear(renderer); // cleans it up
-  SDL_RendererCopy(renderer,
+  SDL_RenderClear(renderer); // cleans it up
+  SDL_RenderCopy(renderer,
                     texture,
                     nullptr, // source rectangle from texture
                     nullptr); // destination rectangle (nullptr = fill window)
-  SDL_RendererPresent(renderer); // show it on screen
+  SDL_RenderPresent(renderer); // show it on screen
 }
 
-
+// returns true if the user wants to exit the game:
+// it takes a pointer so it can actually update our array in chip8
+bool Platform::ProcessInput(uint8_t *keys) {
+  // SDL_Event is a union which means it only allocates memory for the
+  //   biggest thing in the struct, since only one button can be pressed
+  //   at a time
+  SDL_Event event;
+  // there is a queue of things the user typed in that frame, so 
+  //   SDL_PollEvent will put the first thing in the queue into event
+  //   and then remove that item from the queue and keep doing that
+  while (SDL_PollEvent(&event)) { // returns 0 if queue is empty
+    switch (event.type) {
+      case SDL_QUIT: {
+        return true;
+      }
+      case SDL_KEYDOWN: {
+        switch (event.key.keysym.sym) {
+          case SDLK_ESCAPE: {
+            return true;
+          }
+          case SDLK_x: {
+            keys[0] = 1;
+            break;
+          }
+          case SDLK_1: {
+            keys[1] = 1;
+            break;
+          }
+          case SDLK_2: {
+            keys[2] = 1;
+            break;
+          }
+          case SDLK_3: {
+            keys[3] = 1;
+            break;
+          }
+          case SDLK_q: {
+            keys[4] = 1;
+            break;
+          }
+          case SDLK_w: {
+            keys[5] = 1;
+            break;
+          }
+          case SDLK_e: {
+            keys[6] = 1;
+            break;
+          }
+          case SDLK_a: {
+            keys[7] = 1;
+            break;
+          }
+          case SDLK_s: {
+            keys[8] = 1;
+            break;
+          }
+          case SDLK_d: {
+            keys[9] = 1;
+            break;
+          }
+          case SDLK_z: {
+            keys[0xA] = 1;
+            break;
+          }
+          case SDLK_c: {
+            keys[0xB] = 1;
+            break;
+          }
+          case SDLK_4: {
+            keys[0xC] = 1;
+            break;
+          }
+          case SDLK_r: {
+            keys[0xD] = 1;
+            break;
+          }
+          case SDLK_f: {
+            keys[0xE] = 1;
+            break;
+          }
+          case SDLK_v: {
+            keys[0xF] = 1;
+            break;
+          }
+        }
+      } break;
+      case SDL_KEYUP: {
+        switch (event.key.keysym.sym) {
+          case SDLK_x: {
+            keys[0] = 0;
+            break;
+          }
+          case SDLK_1: {
+            keys[1] = 0;
+            break;
+          }
+          case SDLK_2: {
+            keys[2] = 0;
+            break;
+          }
+          case SDLK_3: {
+            keys[3] = 0;
+            break;
+          }
+          case SDLK_q: {
+            keys[4] = 0;
+            break;
+          }
+          case SDLK_w: {
+            keys[5] = 0;
+            break;
+          }
+          case SDLK_e: {
+            keys[6] = 0;
+            break;
+          }
+          case SDLK_a: {
+            keys[7] = 0;
+            break;
+          }
+          case SDLK_s: {
+            keys[8] = 0;
+            break;
+          }
+          case SDLK_d: {
+            keys[9] = 0;
+            break;
+          }
+          case SDLK_z: {
+            keys[0xA] = 0;
+            break;
+          }
+          case SDLK_c: {
+            keys[0xB] = 0;
+            break;
+          }
+          case SDLK_4: {
+            keys[0xC] = 0;
+            break;
+          }
+          case SDLK_r: {
+            keys[0xD] = 0;
+            break;
+          }
+          case SDLK_f: {
+            keys[0xE] = 0;
+            break;
+          }
+          case SDLK_v: {
+            keys[0xF] = 0;
+            break;
+          }
+        }
+      } break;
+    }
+  }
+  return false;
+}
